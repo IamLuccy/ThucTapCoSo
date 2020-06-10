24e4cc09b4ea453455c571fe2bc978aa9c0f875a
Peter Mafercent <dangcongtao1999tn@gmail.com>
2020-06-10 13:40:34 +0700

    fist commit

---
+++ PycharmProjects/Pythonbasic/inputOutput/inputOutInPython.py
@@ -0,0 +1,22 @@
+
+
+print("This is the first program on Python!!")
+
+# input in Python.
+a = input ()
+
+# ép kiểu trong Python.
+# Type Conversion in Python.
+a= int(a)
+print ('Kiểu của a là: ', type(a))
+
+# Declare an Array.
+a = [1, 2, 3.5]
+b = ["abcd", "xy", "aăâ"]
+
+# show an Array's length.
+print ("Length of Array a: ", len(a));
+print ("Length of the first string: ", len(b[0]))
+
+# Add an element to Array "a".
+a.append(2.11)
